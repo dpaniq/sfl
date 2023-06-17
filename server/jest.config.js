b41bcd2c6624f626ce13487f5eb607091a976bc3
Bogdan Gromov <14brother@gmail.com>
2023-06-17 21:52:32 +0300

    move test to tests

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,17 +1,7 @@
 const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig');
 
-console.log([
-  pathsToModuleNameMapper(compilerOptions.paths),
-
-  pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: compilerOptions.baseUrl,
-  }),
-
-  pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
-  }),
-]);
+// https://github.com/kulshekhar/ts-jest/issues/2709
 
 module.exports = {
   globals: {
@@ -22,31 +12,9 @@ module.exports = {
   },
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['<rootDir>/test/fixtures/'],
+  testPathIgnorePatterns: ['<rootDir>/tests/fixtures/'],
 
   roots: ['<rootDir>'],
   modulePaths: [compilerOptions.baseUrl],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
-
-  // -----------------------------------------
-  // The following is an example if you use the projects configuration for jest
-  // projects: [
-  //     // Projects configs can be tricky, some properties seem to be inherited from the
-  //     //     global config and others are overrided. When in doubt, drop it from the top level
-  //     //     and define it specifically for the project. This is more likely if a preset or
-  //     //     a special runner is defined since it will have its own configs it sets.
-  //     // Remember it is JS so you can use variable declarations and spread operators to minimize
-  //     //     duplicate expressions
-  //     {
-  //         displayName: "UNIT",
-  //         preset: "ts-jest",
-  //         runner: "@codejedi365/jest-serial-runner",
-  //         // setupFilesAfterEnv, Must be specified internal to the project, will be ignored if made higher
-  //         setupFilesAfterEnv: ["jest-extended/all"],
-  //         testMatch: ["<rootDir>/tests/**/*.spec.ts"]
-  //     },
-  //     {
-  //         // 2nd project definition
-  //     }
-  // ]
 };
