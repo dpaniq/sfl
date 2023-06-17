3289f4dec6fb273dbcc59b43ea32362759c45b29
Bogdan Gromov <14brother@gmail.com>
2023-06-17 21:05:21 +0300

    fix jest paths alias

--- server/README.md
+++ server/README.md
@@ -6,5 +6,8 @@ This is a very stripped down base project to get started with Node/Express with 
 https://github.com/jmw5598/node-typescript-express-starter/tree/master
 https://medium.com/@fmoessle/typescript-paths-with-ts-node-ts-node-dev-and-jest-671deacf6428
 
+
+
 Google
 resolve ts paths in js.files
+https://levelup.gitconnected.com/path-aliases-with-typescript-in-node-js-230803e3f200
--- server/jest.config.js
+++ server/jest.config.js
@@ -1,5 +1,12 @@
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
+import { compilerOptions } from 'tsconfig';
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testRegex: 'test/.*\.test\.ts$'
+  testRegex: 'test/.*.test.ts$',
+
+  // this enables us to use tsconfig-paths with jest
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
