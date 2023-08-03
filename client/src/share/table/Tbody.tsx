c65109e3f9dee65bfc46cb76e56f8fe70187f5c0
Bogdan Gromov <14brother@gmail.com>
2023-08-03 21:04:58 +0300

    create table component

---
+++ client/src/share/table/Tbody.tsx
@@ -0,0 +1,17 @@
+import { Component, createSignal } from "solid-js";
+
+// type ITable = {
+//   Thead: Component<Thead>
+//   Tbody: Component<Tbody>
+// }
+
+const Thead: Component<T> = () => {
+  return (
+    <tr>
+      thead
+    </tr>
+  )
+}
+
+
+export default Thead;
