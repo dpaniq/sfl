10a3a80e2268b624290372231a0d113b1e042cc9
Bogdan Gromov <14brother@gmail.com>
2023-08-15 11:50:30 +0300

    repository pagination

--- server/src/app/controllers/users.controller.ts
+++ server/src/app/controllers/users.controller.ts
@@ -1,6 +1,7 @@
-import {Request, Response, NextFunction} from 'express';
-import {IRepository} from '../repositories';
+import {NextFunction, Request, Response} from 'express';
 import {User} from '../data';
+import {IRepository} from '../repositories';
+import {isNumber} from '../utils/number';
 
 export class UsersController {
   private readonly _repository: IRepository<User>;
@@ -16,6 +17,25 @@ export class UsersController {
   ): Promise<Response> {
     return this._repository
       .findAll()
+      .then((users) => response.status(200).send(users))
+      .catch((error) => response.status(500).send({error: error}));
+  }
+
+  public async getUsersList(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ): Promise<Response> {
+    console.log(request.query);
+
+    const take = isNumber(request.query.take) ? Number(request.query.take) : undefined;
+    const skip = isNumber(request.query.skip) ? Number(request.query.skip) : undefined;
+    const searchQuery = request.query.searchQuery as string;
+
+    console.log(take, skip, searchQuery);
+
+    return this._repository
+      .findAllPagination({take, skip, searchQuery})
       .then((users) => response.status(200).send(users))
       .catch((error) => response.status(500).send({error: error}));
   }
--- server/src/app/repositories/repository.interface.ts
+++ server/src/app/repositories/repository.interface.ts
@@ -1,4 +1,9 @@
 export interface IRepository<T> {
   findAll(): Promise<T[]>;
   findOne(id: string): Promise<T | undefined>;
+  findAllPagination(queryParams: {
+    take?: number;
+    skip?: number;
+    searchQuery?: string;
+  }): Promise<Paginate<T>>;
 }
