cbbe1015298f1a8040ad29f738b42194a5bddd3c
Bogdan Gromov <14brother@gmail.com>
2023-08-18 19:47:58 +0300

    players infinite table fixed 2

--- client/src/pages/Players.tsx
+++ client/src/pages/Players.tsx
@@ -1,6 +1,6 @@
 import InfiniteScroll from 'solid-infinite-scroll';
 import {JSX, createResource, createSignal, type Component} from 'solid-js';
-import BasicTable from '../components/UserTable';
+import PlayersInfiniteTable from '../components/PlayersInfiniteTable';
 import {TableCell, TableRow} from '@suid/material';
 // const handleSearchInput: JSX.EventHandler<HTMLInputElement, InputEvent> = (event) => {
 //   console.log(event.currentTarget.value);
@@ -15,67 +15,27 @@ import {TableCell, TableRow} from '@suid/material';
 
 // const fetchApi = async () => await (await fetch('https://api.example.com/list')).json();
 // const fetchApi = async () => listPromise;
-const fetchApi = async (): Promise<{data: IPlayer[]; count: number}> => {
-  console.log('FETCH API FOR PLAYERS');
-  const x = await (await fetch('mock/list.json')).json();
-  console.log((x as any)?.count);
-  return x;
-};
-
-const listNumber = 5;
 
 // https://libraries.io/npm/solid-infinite-scroll
 
 export default function Players<Component>({number}) {
-  const [api] = createResource(fetchApi);
-
-  const [scrollIndex, setScrollIndex] = createSignal(listNumber);
-  const scrollNext = () => setScrollIndex(Math.min(scrollIndex() + listNumber, api()?.data.length));
-
-  const [searchString, setSearchString] = createSignal('');
+  const [searchQuery, setSearchQuery] = createSignal('');
 
   const handleSearchInput: JSX.EventHandler<HTMLInputElement, InputEvent> = (event) => {
     console.log(event.currentTarget.value);
-    setSearchString(event.currentTarget.value);
+    setSearchQuery(event.currentTarget.value);
   };
 
   return (
     <section class="players">
-      <br />
-      <pre>
-        scrollIndex: {scrollIndex()}; hasMore={scrollIndex() < api()?.count}
-      </pre>
-      <hr />
-
-      <hr />
-      <br />
       <input
         placeholder="Search players..."
         class="players-input"
         type="text"
         onInput={handleSearchInput}
-        value={searchString()}
+        value={searchQuery()}
       />
-      <BasicTable searchString={searchString}>
-        <InfiniteScroll
-          each={api()?.data.slice(0, scrollIndex())}
-          hasMore={scrollIndex() < api()?.count}
-          next={scrollNext}
-        >
-          {(row, index) => (
-            <TableRow sx={{'&:last-child td, &:last-child th': {border: 0}}}>
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell align="right">{row.email}</TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.nickname}</TableCell>
-              <TableCell align="right">{row.surname}</TableCell>
-              <TableCell align="right">{row.age}</TableCell>
-            </TableRow>
-          )}
-        </InfiniteScroll>
-      </BasicTable>
+      <PlayersInfiniteTable searchQuery={searchQuery} />
     </section>
   );
 }
