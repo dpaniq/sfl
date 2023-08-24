00cd8ede6635425e143070808f1a470777694637
Bogdan Gromov <14brother@gmail.com>
2023-08-24 18:46:17 +0300

    create page

---
+++ client/src/pages/CreateMatch.tsx
@@ -0,0 +1,14 @@
+import {type Component} from 'solid-js';
+
+export const CreateMatch: Component = () => {
+  return (
+    <div>
+      <div>About</div>
+      <div>About</div>
+      <div>About</div>
+      <div>About</div>
+      <div>About</div>
+      <div>About</div>
+    </div>
+  );
+};
