ed6fae756bc527b5d8b50881bcb4574897006312
Bogdan Gromov <14brother@gmail.com>
2023-10-14 18:18:24 +0300

    create side menu trigger

--- client/src/shared/ui/core/aside/aside.component.ts
+++ client/src/shared/ui/core/aside/aside.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 
 import { LINKS } from './constants';
@@ -8,7 +10,7 @@ import { LINKS } from './constants';
 @Component({
   selector: 'app-aside',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatIconModule],
+  imports: [CommonModule, RouterModule, MatIconModule, MatListModule],
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.css'],
 })
