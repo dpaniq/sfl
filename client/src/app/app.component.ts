8dcd2f5cd582b10fab25cfd6f54c076886952743
Bogdan Gromov <14brother@gmail.com>
2023-10-14 16:32:52 +0300

    create header toolbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,11 +3,22 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AsideComponent } from 'src/shared/ui/core/aside';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AsideComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    AsideComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+  ],
 })
 export class AppComponent {}
