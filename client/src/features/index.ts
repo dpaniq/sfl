956d63f01ca87821cf9c3456f83254fd845f6fc7
Bogdan Gromov <14brother@gmail.com>
2023-10-18 20:41:44 +0300

    add captains table to captains page

--- client/src/features/index.ts
+++ client/src/features/index.ts
@@ -1,9 +1,11 @@
 import { CaptainToAddComponent } from './captain-to-add/captain-to-add.component';
 import { CaptainsSelectionComponent } from './captains-selection/captains-selection.component';
 import { PlayersSelectionComponent } from './players-selection/players-selection.component';
+import { CaptainsTableComponent } from './captains-table/captains-table.component';
 
 export {
   CaptainToAddComponent,
   CaptainsSelectionComponent,
   PlayersSelectionComponent,
+  CaptainsTableComponent,
 };
