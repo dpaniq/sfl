905ca6463da9bfb3cbde4810de034a7e0a254806
Bogdan Gromov <14brother@gmail.com>
2023-10-23 20:43:27 +0300

    add captain admin widget

--- client/src/pages/new-match/new-match.component.ts
+++ client/src/pages/new-match/new-match.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NewMatchWidgetComponent } from 'src/widgets/new-match';
+import { NewMatchWidgetComponent } from '@widgets';
 
 @Component({
   standalone: true,
---
+++ client/src/widgets/index.ts
@@ -0,0 +1,5 @@
+import { CaptainsAdminWidgetComponent } from './captains-admin-widget/captains-admin-widget.component';
+
+import { NewMatchWidgetComponent } from './new-match/new-match.widget.component';
+
+export { CaptainsAdminWidgetComponent, NewMatchWidgetComponent };
