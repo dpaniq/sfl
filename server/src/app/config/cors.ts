99b73a5a9449a7e71feb5cd4255fe0132c315075
Bogdan Gromov <14brother@gmail.com>
2023-11-19 17:29:17 +0200

    migrate http to https

---
+++ server/src/app/config/cors.ts
@@ -0,0 +1,12 @@
+import {SERVER_HOSTNAME, SERVER_PORT, CLIENT_HOSTNAME, CLIENT_PORT} from '@env';
+
+export const CORS_CONFIGURATION = {
+  origin: [
+    `https://${SERVER_HOSTNAME}:${SERVER_PORT}`,
+    `https://${CLIENT_HOSTNAME}:${CLIENT_PORT}`,
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true,
+};
