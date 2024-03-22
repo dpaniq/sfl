15f9ba6f3e803c8fbf594f0c1ce04d5ee0676428
Bogdan Gromov <14brother@gmail.com>
2024-03-22 17:38:35 +0200

    tech: clean games utils

--- client/src/entities/games/utils.ts
+++ client/src/entities/games/utils.ts
@@ -1,20 +1,7 @@
 import { getISOWeeksInYear, isSaturday, previousSaturday } from 'date-fns';
 
-import { totalWeeksByYear } from '@entities/utils/date';
-import { GamePlayer } from './store/new-game.store';
-
 export const totalWeeksByYear = (date: Date) => getISOWeeksInYear(date);
 
 export const getLastSaturday = isSaturday(new Date())
   ? new Date()
   : previousSaturday(new Date());
-
-export const isCurrentCaptain = ({
-  team,
-  disable,
-  isCaptain,
-}: GamePlayer): boolean => {
-  return;
-};
-
-const totalWeeksOfYear = totalWeeksByYear(new Date());
